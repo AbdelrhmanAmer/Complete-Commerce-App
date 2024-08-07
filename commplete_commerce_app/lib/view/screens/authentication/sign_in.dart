af00f09914feb44738e33c85ed3e4d3b62ad813d
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-08-07 14:07:49 +0300

    CircularProcessIndicator in while Signing In

--- commplete_commerce_app/lib/view/screens/authentication/sign_in.dart
+++ commplete_commerce_app/lib/view/screens/authentication/sign_in.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../controller/authentication/sign_in_controller.dart';
+import '../../../core/class/status_request.dart';
 import '../../../core/constant/constants.dart';
 import '../../../core/constant/color.dart';
 import '../../widgets/custom_button.dart';
@@ -14,74 +15,76 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var controller = Get.put(SignInController());
-
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text(
-            'Sign In',
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall!
-                .copyWith(color: Colors.black.withOpacity(.7)),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
+        child: Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Sign In',
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .copyWith(color: Colors.black.withOpacity(.7)),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: size.height * .01),
-                SizedBox(
-                  height: size.height * .2,
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: CircleAvatar(
-                      child: Image.asset('assets/images/logo.png', scale: 1),
-                    ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+      ),
+      body: GetBuilder<SignInController>(builder: (controller) {
+        return controller.statusRequest.value == StatusRequest.loading
+            ? const Center(child: CircularProgressIndicator())
+            : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: size.height * .01),
+                      SizedBox(
+                        height: size.height * .2,
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: CircleAvatar(
+                            child:
+                                Image.asset('assets/images/logo.png', scale: 1),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: size.height * .02),
+                      Text('Welcome Back',
+                          style: Theme.of(context).textTheme.displayLarge),
+                      SizedBox(height: size.height * .02),
+                      Text(
+                        'Sign in with your email and password\n or continue with social media',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelSmall!
+                            .copyWith(height: 1.5),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: size.height * .03),
+                      SignInForm(controller: controller),
+                      SizedBox(height: size.height * .02),
+                      CustomButton(
+                        backgroundColor: AppColor.primaryColor,
+                        foregroundColor: Colors.white,
+                        text: 'Sign In',
+                        press: controller.signIn,
+                        widthRatio: .85,
+                      ),
+                      SizedBox(height: size.height * .02),
+                      const SocialIcons(),
+                      SizedBox(height: size.height * .01),
+                      AccountPrompt(
+                        promptText: 'Don\'t have an account? ',
+                        actionText: 'Sign Up',
+                        onActionPressed: controller.goToSignUp,
+                      ),
+                      SizedBox(height: size.height * .01),
+                    ],
                   ),
                 ),
-                SizedBox(height: size.height * .02),
-                Text('Welcome Back',
-                    style: Theme.of(context).textTheme.displayLarge),
-                SizedBox(height: size.height * .02),
-                Text(
-                  'Sign in with your email and password\n or continue with social media',
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelSmall!
-                      .copyWith(height: 1.5),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: size.height * .03),
-                SignInForm(controller: controller),
-                SizedBox(height: size.height * .02),
-                CustomButton(
-                  backgroundColor: AppColor.primaryColor,
-                  foregroundColor: Colors.white,
-                  text: 'Sign In',
-                  press: controller.signIn,
-                  widthRatio: .85,
-                ),
-                SizedBox(height: size.height * .02),
-                const SocialIcons(),
-                SizedBox(height: size.height * .01),
-                AccountPrompt(
-                  promptText: 'Don\'t have an account? ',
-                  actionText: 'Sign Up',
-                  onActionPressed: controller.goToSignUp,
-                ),
-                SizedBox(height: size.height * .01),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+              );
+      }),
+    ));
   }
 }
