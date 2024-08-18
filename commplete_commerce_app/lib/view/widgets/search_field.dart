c459fb07f3d45bded5200c4e8b33d5e3004bb920
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-08-18 07:44:30 +0300

    Home (SearchField) 2

---
+++ commplete_commerce_app/lib/view/widgets/search_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../core/constant/color.dart';
+
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var outlineInputBorder = OutlineInputBorder(
+        borderSide: BorderSide.none, borderRadius: BorderRadius.circular(12));
+    return TextField(
+      decoration: InputDecoration(
+          prefixIcon: const Padding(
+            padding: EdgeInsets.only(
+                left: 20.0, right: 20), // Adjust padding as needed
+            child: Icon(CupertinoIcons.search),
+          ),
+          hintText: 'Search the entire shop',
+          hintStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Get.isDarkMode
+                    ? AppColor.lightDarkText
+                    : AppColor.lightWhiteText,
+                fontWeight: FontWeight.bold,
+              ),
+          border: outlineInputBorder,
+          enabledBorder: outlineInputBorder,
+          disabledBorder: outlineInputBorder,
+          focusedBorder: outlineInputBorder,
+          filled: true,
+          fillColor: Get.isDarkMode
+              ? AppColor.lightDarkBackground
+              : AppColor.lightwhiteBackground),
+    );
+  }
+}
